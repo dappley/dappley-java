9ad02f8e5ea937bf8dba9002d4a233c7c7a9c632
yangdong <ahuyangdong@163.com>
2018-12-06 22:46:08 +0800

    add welcome page and perfect activity enter animation

---
+++ android/DappleyAndroid/app/src/main/java/com/dappley/android/WelcomeActivity.java
@@ -0,0 +1,37 @@
+package com.dappley.android;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_welcome);
+
+        jumpToMain();
+    }
+
+    private void jumpToMain() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                openMain();
+            }
+        }, 3000);
+    }
+
+    private void openMain() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}

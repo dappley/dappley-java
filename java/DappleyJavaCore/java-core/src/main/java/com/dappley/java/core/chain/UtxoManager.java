12abc6ce771044e431ad6be9142833a41a363103
xiaoyoulei <xyl1932432873@163.com>
2021-03-26 17:15:18 +0800

    Optimize getUtxo in sendTransction function

--- java/DappleyJavaCore/java-core/src/main/java/com/dappley/java/core/chain/UtxoManager.java
+++ java/DappleyJavaCore/java-core/src/main/java/com/dappley/java/core/chain/UtxoManager.java
@@ -41,31 +41,13 @@ public class UtxoManager {
         });
         List<Utxo> spendables = new ArrayList<>();
         BigInteger accumulated = BigInteger.ZERO;
-        for (int i = 0, size = utxos.size(); i < size; i++) {
-            if (i == 50){
-               break;
-            }
-            if (utxos.get(i) == null) {
-                continue;
-            }
-            accumulated = accumulated.add(utxos.get(i).getAmount());
-            spendables.add(utxos.get(i));
-            if (accumulated.compareTo(amount) >= 0) {
-                break;
-            }
-        }
-        if (accumulated.compareTo(amount) >= 0) {
-            return spendables;
-        }
-        accumulated.setBit(0);
-        spendables.clear();
         for (int i = utxos.size()-1; i >=utxos.size()-50 && i>=0; i--) {
             if (utxos.get(i) == null) {
                 continue;
             }
             accumulated = accumulated.add(utxos.get(i).getAmount());
             spendables.add(utxos.get(i));
-            if (accumulated.compareTo(amount) >= 0) {
+            if (accumulated.compareTo(amount) >= 0 && utxos.size() < 100) {
                 break;
             }
         }
